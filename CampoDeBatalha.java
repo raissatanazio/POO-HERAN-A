f7af03111dd431dc66d1347063dd9e1dacb4849a
raissatanazio <raissattanazio@gmail.com>
2025-08-29 22:23:30 -0300

    Create CampoDeBatalha.java

---
+++ CampoDeBatalha.java
@@ -0,0 +1,11 @@
+public class CampoDeBatalha{
+    public static void main(String[] args){
+        Mago p1 = new Mago("Merlin");
+        
+        p1.estaVivo();
+        p1.ativarMarcadeCaim();
+        p1.mostrarStatus();
+
+
+    }
+}
