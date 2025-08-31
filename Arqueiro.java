fcada1a8019639097352cec33ff6c7b531cc2ddb
raissatanazio <raissattanazio@gmail.com>
2025-08-31 10:09:50 -0300

    base aqueiro

---
+++ Arqueiro.java
@@ -0,0 +1,16 @@
+public class Arqueiro extends Personagem {
+    private int flechas;
+    private final int PRECISAO = 3;
+    private final int AGILIDADE = 3; //final = valor constante
+
+    public Arqueiro(String nome){
+        super(nome); //super chama o contrutor da mãe
+        this.flechas = 25;
+        setVida(100);
+        setAtaque(12);
+        setDefesa(10);
+    }
+
+
+
+}
