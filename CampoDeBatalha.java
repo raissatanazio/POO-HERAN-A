2c2f0d7192ea2e3ff045ad74ddc5f4bcecc233b1
raissatanazio <raissattanazio@gmail.com>
2025-09-01 21:02:18 -0300

    Update CampoDeBatalha.java

--- CampoDeBatalha.java
+++ CampoDeBatalha.java
@@ -3,6 +3,17 @@ public class CampoDeBatalha{
         Mago merlin = new Mago("Merlin");
         Arqueiro alec = new Arqueiro("Alec");
 
-        
+        merlin.estaVivo();
+        alec.estaVivo();
+        merlin.atacar(alec);
+        merlin.lancarBolaDeFogo(alec);
+        alec.chuvaDeFlechas(merlin);
+        alec.usarFlechaParaCombateCorpoACorpo(merlin);
+        alec.atacar(merlin);
+        merlin.atacar(alec);
+        merlin.ativarMarcadeCaim();
+        alec.atacar(merlin);
+        merlin.receberAtaque(alec, 15);
+        merlin.mostrarStatus();
     }
 }
